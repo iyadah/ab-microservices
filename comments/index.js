25ac04eaa75b0ba7a0af9e629a49e3eb7c8f1c29
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-08-04 15:15:42 +0300

    add event bus

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const cors = require('cors');
 
@@ -14,14 +15,27 @@ app.get('/posts/:id/comments', (req, res)=>{
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res)=>{
+app.post('/posts/:id/comments', async (req, res)=>{
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body;
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({id: commentId, content});
     commentsByPostId[req.params.id] = comments;
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    })
     res.status(201).send(comments);
 
+});
+
+app.post('/events', async (req, res)=>{
+    await console.log('Received event',  req.body.type);
+    res.send({});
 });
 
 app.listen(4001, ()=>{
